2cc04f42b6ac4445fc38bf8a8bca04788b24d869
dcmunhoz <dc.munhoz@hotmail.com>
2020-06-19 21:26:21 -0300

    Initial commit: project builded

---
+++ prjImporta_LayoutPTU/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace prjImporta_LayoutPTU
+{
+    partial class frmLayout_PTU
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmLayout_PTU));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.txtCaminhoArquivoImportado = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.lblStatus = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.lblDados = new System.Windows.Forms.ToolStripStatusLabel();
+            this.btnLocalizar = new System.Windows.Forms.Button();
+            this.btnImportar = new System.Windows.Forms.Button();
+            this.btnApagar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(85, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(154, 76);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // txtCaminhoArquivoImportado
+            // 
+            this.txtCaminhoArquivoImportado.Enabled = false;
+            this.txtCaminhoArquivoImportado.Font = new System.Drawing.Font("Trebuchet MS", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCaminhoArquivoImportado.Location = new System.Drawing.Point(17, 168);
+            this.txtCaminhoArquivoImportado.Name = "txtCaminhoArquivoImportado";
+            this.txtCaminhoArquivoImportado.Size = new System.Drawing.Size(281, 21);
+            this.txtCaminhoArquivoImportado.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Trebuchet MS", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(76, 91);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(178, 29);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Layout PTU 5.1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(14, 144);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(142, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Caminho do arquivo";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lblStatus,
+            this.toolStripStatusLabel1,
+            this.lblDados});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 373);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Padding = new System.Windows.Forms.Padding(1, 0, 16, 0);
+            this.statusStrip1.Size = new System.Drawing.Size(316, 22);
+            this.statusStrip1.TabIndex = 4;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(39, 17);
+            this.lblStatus.Tag = "Status";
+            this.lblStatus.Text = "Status";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(10, 17);
+            this.toolStripStatusLabel1.Text = "|";
+            // 
+            // lblDados
+            // 
+            this.lblDados.Name = "lblDados";
+            this.lblDados.Size = new System.Drawing.Size(40, 17);
+            this.lblDados.Text = "Dados";
+            // 
+            // btnLocalizar
+            // 
+            this.btnLocalizar.Location = new System.Drawing.Point(103, 195);
+            this.btnLocalizar.Name = "btnLocalizar";
+            this.btnLocalizar.Size = new System.Drawing.Size(114, 50);
+            this.btnLocalizar.TabIndex = 5;
+            this.btnLocalizar.Text = "Localizar Arquivo";
+            this.btnLocalizar.UseVisualStyleBackColor = true;
+            this.btnLocalizar.Click += new System.EventHandler(this.btnLocalizar_Click);
+            // 
+            // btnImportar
+            // 
+            this.btnImportar.Location = new System.Drawing.Point(103, 251);
+            this.btnImportar.Name = "btnImportar";
+            this.btnImportar.Size = new System.Drawing.Size(114, 50);
+            this.btnImportar.TabIndex = 6;
+            this.btnImportar.Text = "Importar";
+            this.btnImportar.UseVisualStyleBackColor = true;
+            this.btnImportar.Click += new System.EventHandler(this.btnImportar_Click);
+            // 
+            // btnApagar
+            // 
+            this.btnApagar.Location = new System.Drawing.Point(103, 307);
+            this.btnApagar.Name = "btnApagar";
+            this.btnApagar.Size = new System.Drawing.Size(114, 50);
+            this.btnApagar.TabIndex = 7;
+            this.btnApagar.Text = "Apagar dados";
+            this.btnApagar.UseVisualStyleBackColor = true;
+            this.btnApagar.Click += new System.EventHandler(this.btnApagar_Click);
+            // 
+            // frmLayout_PTU
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(316, 395);
+            this.Controls.Add(this.btnApagar);
+            this.Controls.Add(this.btnImportar);
+            this.Controls.Add(this.btnLocalizar);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtCaminhoArquivoImportado);
+            this.Controls.Add(this.pictureBox1);
+            this.Font = new System.Drawing.Font("Trebuchet MS", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.MaximizeBox = false;
+            this.Name = "frmLayout_PTU";
+            this.Text = "Importar Layout PTU 5.1";
+            this.Load += new System.EventHandler(this.frmLayout_PTU_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox txtCaminhoArquivoImportado;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel lblStatus;
+        private System.Windows.Forms.Button btnLocalizar;
+        private System.Windows.Forms.Button btnImportar;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripStatusLabel lblDados;
+        private System.Windows.Forms.Button btnApagar;
+    }
+}
+
---
+++ prjImporta_LayoutPTU/Form1.cs
@@ -0,0 +1,321 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.OleDb;
+using System.IO;
+
+namespace prjImporta_LayoutPTU {
+
+    public partial class frmLayout_PTU : Form {
+        // Criar conexão com o banco
+        private OleDbConnection msConnection = new OleDbConnection();
+        private string conStringSeven = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\\Users\\" + Environment.UserName.ToString() + "\\AppData\\Local\\cardPresso\\DATABASE\\internalDatabase.mdb;";
+        private string conStringXP = @"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=C:\\Documents and Settings\\" + Environment.UserName.ToString() + "\\Configurações locais\\Dados de aplicativos\\cardPresso\\DATABASE\\internalDatabase.mdb;";
+
+
+        // Auxiliares
+        public OpenFileDialog OFDialog = new OpenFileDialog();
+
+        // Função para verificar registros existentes,
+        public void verificaRegistros() {
+            try {
+                msConnection.Open();
+                OleDbCommand msComand = new OleDbCommand();
+                msComand.Connection = msConnection;
+                msComand.CommandText = "select * from Layout_PTU";
+
+                OleDbDataReader reader = msComand.ExecuteReader();
+
+                int count = 0;
+                while (reader.Read()) {
+                    count++;
+                }
+
+                if (count >= 1) {
+                    lblDados.Text = "Quantidade de registros no banco: " + count.ToString();
+                } else {
+                    lblDados.Text = "Não existe registros no banco.";
+                }
+
+                msConnection.Close();
+
+            } catch (Exception ex) {
+                MessageBox.Show("Erro ao verificar registros, mensagem do banco: " + ex.ToString());
+            }
+        }
+
+        public frmLayout_PTU() {
+            InitializeComponent();
+            // Verifica a versão do Windows e atribui a String de conexão para aquele OS .
+            if (int.Parse(Environment.OSVersion.ToString().Substring(21, 1)) >= 6) // Vindows 7 e superiores
+            {
+                msConnection.ConnectionString = conStringSeven; // Inicializa string de conexão   
+            }
+            else // Vista e inferiores
+            {
+                msConnection.ConnectionString = conStringXP;
+            }
+        }
+
+
+        private void frmLayout_PTU_Load(object sender, EventArgs e) {
+
+            // Testar conexão com o banco
+            try {
+                msConnection.Open();
+                lblStatus.Text = "Conectado";
+                msConnection.Close();
+
+                //Verificar se existe registros
+                verificaRegistros();
+            }
+            catch (Exception ex) {
+                lblStatus.Text = "Não conectado";
+                MessageBox.Show("Não foi possivel fazer a conexão com o banco,\n verifique se o caminho esta correto. \n \n O aplicativo será encerrado.",
+                    "Erro !",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+
+                MessageBox.Show("Erro de banco: \n \n" + ex.Message, "Error !", MessageBoxButtons.OK, MessageBoxIcon.Error) ;
+
+                btnLocalizar.Enabled = false;
+                btnApagar.Enabled = false;
+                btnImportar.Enabled = false;
+
+                this.Close();
+            }
+
+        }
+
+        private void btnLocalizar_Click(object sender, EventArgs e) {
+            // Pega o caminho do arquivo verificando se esta no formato .TXT
+            string nomeArquivo = "";
+            do {
+                // Pega o caminho do arquivo e o nome
+                if (OFDialog.ShowDialog() != DialogResult.Cancel) {
+                    txtCaminhoArquivoImportado.Text = OFDialog.FileName;
+                    string[] path = txtCaminhoArquivoImportado.ToString().Split('\\'); // Quebra o caminho do arquivo em um vetor para pegar somente o index que conter o nome.
+                    nomeArquivo = path[path.Length - 1]; //pega o ultimo index que é referente ao nome do arquivo.
+                    //nomeArquivo = OFDialog.
+                }
+                else {
+                    txtCaminhoArquivoImportado.Text = "";
+                    nomeArquivo = "";
+                    break;
+                }
+
+                // Se o arquivo for diferente exibe a mensagem de erro.
+                if (nomeArquivo.Substring(nomeArquivo.Length - 3, 3) != "TXT" && nomeArquivo.Substring(nomeArquivo.Length - 3, 3) != "txt") {
+                    //MessageBox.Show(nomeArquivo);
+                    MessageBox.Show(null, "Arquivo no formato invalido. \nO formato do arquivo deve ser .TXT !!!", "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+            } while (nomeArquivo.Substring(nomeArquivo.Length - 3, 3) != "TXT" && nomeArquivo.Substring(nomeArquivo.Length - 3, 3) != "txt");
+        }
+
+        private void btnImportar_Click(object sender, EventArgs e) {
+            // Verifica se o campo que contem o caminho do arquivo esta preenchido.
+            if (txtCaminhoArquivoImportado.Text == "") {
+                MessageBox.Show(null, "Favor informar o caminho do arquivo.", "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            // Atribuir o arquivo a uma variavel do sistema.
+            string caminhoArquivo = txtCaminhoArquivoImportado.Text;
+            string[] linhasArquivo = File.ReadAllLines(caminhoArquivo); // Vetor que pega cada linha do arquivo.
+            int qtdeLinhasArquivo = File.ReadAllLines(caminhoArquivo).Count();
+
+            /*======================================= Começar a Importação =======================================*/
+
+            // Variaveis com os dados a serem importados.
+            int id;
+            string plano, tipo_contrato, cartao_benef, dt_nascimento, acomodacao, dt_vigencia,
+                    dt_validade, nome_benef, rede, tipo_prestador, atendimento, regulamentacao,
+                    abrangencia, via, cobertura, contratante, segmentacao, trilha1, trilha2,
+                    trilha3, cod_ans_operadora, cod_ans_produto, cod_ans_op_cont, desc_abrangencia, carencia1,
+                    carencia2, carencia3, carencia4, carencia5, carencia6, carencia7, carencia8, sem_carencia,
+                    nome_administradora, cnSaude;
+            string carencia11, carencia21, carencia31, carencia41, carencia51, carencia61, carencia71, carencia81;
+
+            // inserir no banco
+            try {
+                msConnection.Open();
+                OleDbCommand msComando = new OleDbCommand();
+                msComando.Connection = msConnection;
+
+                // Laço para pegar os valores e atribuir as variaveis.
+                for (int i = 0; i < qtdeLinhasArquivo; i++) {
+                    // ID
+                    id = i + 1;
+
+                    /* =================   Frente  ================= */
+                    /* ================= Cabeçalho ================= */
+                    /* =================  Linha 1  ================= */
+
+                    // Plano
+                    plano = linhasArquivo[i].Substring(0, 60).ToString();
+
+                    /* ================= Linha 2 ================= */
+                    // Tipo Contrato
+                    tipo_contrato = linhasArquivo[i].Substring(60, 30).ToString().ToUpper();
+
+                    /* ================= Corpo Cartao ================= */
+                    /* =================    Linha 1   ================= */
+
+                    // Cartão Beneficiario
+                    cartao_benef = linhasArquivo[i].Substring(90, 20).ToString();
+
+                    /* =================    Linha 2   ================= */
+
+                    // Data Nascimento
+                    dt_nascimento = linhasArquivo[i].Substring(110, 14).ToString();
+
+                    // Acomodação
+                    acomodacao = linhasArquivo[i].Substring(124, 17).ToString().ToUpper();
+
+                    // Vigencia
+                    dt_vigencia = linhasArquivo[i].Substring(141, 14).ToString();
+
+                    // Validade Cartao
+                    dt_validade = linhasArquivo[i].Substring(155, 10).ToString();
+
+                    /* =================    Linha 3   ================= */
+
+                    // Nome Beneficiario
+                    nome_benef = linhasArquivo[i].Substring(165, 35).ToString();
+
+                    // Rede Preferenciada
+                    rede = linhasArquivo[i].Substring(200, 4).ToString();
+
+                    // Tipo Prestador
+                    tipo_prestador = linhasArquivo[i].Substring(205, 8).ToString();
+
+                    /* =================    Linha 4   ================= */
+
+                    // Local Atendimento
+                    atendimento = linhasArquivo[i].Substring(213, 9).ToString();
+
+                    // Tipo Regulamentação 
+                    regulamentacao = linhasArquivo[i].Substring(222, 23).ToString().ToUpper();
+
+                    // Abrangencia
+                    abrangencia = linhasArquivo[i].Substring(245, 21).ToString().ToUpper();
+
+                    // Via
+                    via = linhasArquivo[i].Substring(266, 2).ToString();
+
+                    /* =================    Linha 5   ================= */
+
+                    // CPT
+                    cobertura = linhasArquivo[i].Substring(268, 31).ToString();
+
+                    // Empresa
+                    contratante = linhasArquivo[i].Substring(299, 18).ToString();
+
+                    /* =================    Linha 6   ================= */
+
+                    // Segmentação
+                    segmentacao = linhasArquivo[i].Substring(317, 56).ToString();
+
+                    /* =================   Verso  ================= */
+                    /* ================= Trilha 1 ================= */
+                    trilha1 = linhasArquivo[i].Substring(373, 28).ToString();
+
+                    /* ================= Trilha 2 ================= */
+                    trilha2 = linhasArquivo[i].Substring(401, 40).ToString();
+
+                    /* ================= Trilha 3 ================= */
+                    trilha3 = linhasArquivo[i].Substring(441, 104).ToString();
+
+                    /* =================   Informações  ================= */
+                    // Codigo da operadora na ANS
+                    cod_ans_operadora = linhasArquivo[i].Substring(545, 6).ToString();
+
+                    // Codigo do produto na ANS
+                    cod_ans_produto = linhasArquivo[i].Substring(551, 30).ToString();
+
+                    // NUMERO REGISTRO ANS NA OPERADORA CONTRATADA
+                    cod_ans_op_cont = linhasArquivo[i].Substring(581, 26).ToString();
+
+                    // Descrição Abrangencia
+                    desc_abrangencia = linhasArquivo[i].Substring(607, 180).ToString();
+
+                    // Mensagens Carencia
+                    carencia1 = linhasArquivo[i].Substring(787, 60).ToString();
+                    carencia11 = linhasArquivo[i].Substring(847, 8).ToString();
+                    carencia2 = linhasArquivo[i].Substring(855, 60).ToString();
+                    carencia21 = linhasArquivo[i].Substring(915, 8).ToString();
+                    carencia3 = linhasArquivo[i].Substring(923, 60).ToString();
+                    carencia31 = linhasArquivo[i].Substring(983, 8).ToString();
+                    carencia4 = linhasArquivo[i].Substring(991, 60).ToString();
+                    carencia41 = linhasArquivo[i].Substring(1051, 8).ToString();
+                    carencia5 = linhasArquivo[i].Substring(1059, 60).ToString();
+                    carencia51 = linhasArquivo[i].Substring(1119, 8).ToString();
+                    carencia6 = linhasArquivo[i].Substring(1127, 60).ToString();
+                    carencia61 = linhasArquivo[i].Substring(1187, 8).ToString();
+                    carencia7 = linhasArquivo[i].Substring(1195, 60).ToString();
+                    carencia71 = linhasArquivo[i].Substring(1255, 8).ToString();
+                    carencia8 = linhasArquivo[i].Substring(1263, 60).ToString();
+                    carencia81 = linhasArquivo[i].Substring(1323, 8).ToString();
+                    
+                    
+                    // Sem Carencia
+                    sem_carencia = linhasArquivo[i].Substring(1331, 23).ToString();
+
+                    // Nome administradora
+                    nome_administradora = linhasArquivo[i].Substring(1354, 25).ToString();
+
+                    // Cartão Nacional Saúde
+                    cnSaude = linhasArquivo[i].Substring(1379, 15).ToString();
+
+
+                    // Insrir os valores
+                    msComando.CommandText = "insert into Layout_PTU values(" + id + " , '" + plano + "', '" + tipo_contrato + "', '" +
+                                            cartao_benef + "', '" + dt_nascimento + "', '" + acomodacao + "', '" + dt_vigencia + "', '" + dt_validade + "', '" + nome_benef
+                                            + "', '" + rede + "', '" + tipo_prestador + "', '" + atendimento + "', '" + regulamentacao + "', '" + abrangencia + "', '" + via
+                                            + "', '" + cobertura + "', '" + contratante + "', '" + segmentacao + "', '" + trilha1 + "', '" + trilha2 + "', '" + trilha3
+                                            + "', '" + cod_ans_operadora + "', '" + cod_ans_produto + "', '" + cod_ans_op_cont + "', '" + desc_abrangencia + "', '" + carencia1
+                                            + "', '" + carencia11 + "', '" + carencia2 + "', '" + carencia21 + "', '" + carencia3 + "', '" + carencia31
+                                            + "', '" + carencia4 + "', '" + carencia41 + "', '" + carencia5 + "', '" + carencia51 + "', '" + carencia6 + "', '" +carencia61
+                                            + "', '" + carencia7 + "', '" + carencia71 + "', '" + carencia8 + "', '" + carencia81 + "', '" + sem_carencia + "', '" + nome_administradora + "', '"+ cnSaude + "');";
+
+                    msComando.ExecuteNonQuery();
+                }
+                msConnection.Close();
+            }
+            catch (Exception ex) {
+                MessageBox.Show("Erro na importação, mesagem do banco: " + ex.ToString());
+            } finally {
+                MessageBox.Show(null, "Arquivo importado com sucesso !", "Informação", MessageBoxButtons.OK,MessageBoxIcon.Information);
+            }
+
+            //Verificar se existe registros
+            verificaRegistros();
+        }
+
+        private void btnApagar_Click(object sender, EventArgs e) {
+
+            try {
+                OleDbCommand msComand = new OleDbCommand();
+                msConnection.Open();
+                msComand.Connection = msConnection;
+                msComand.CommandText = "delete from Layout_PTU";
+
+                msComand.ExecuteNonQuery();
+
+                msConnection.Close();
+                MessageBox.Show("Base de dados atual apagada.");
+
+            } catch(Exception ex) {
+                MessageBox.Show("Erro: " + ex.ToString());
+            }
+
+            //Verificar se existe registros
+            verificaRegistros();
+        }
+    }
+}
